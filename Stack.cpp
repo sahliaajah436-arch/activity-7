76e696fa63d28b8e8717d54fd8c2a4dc5c08b59b
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-05-14 11:19:11 +0700

    Menambahkan exit dan finalisasi program

--- Stack.cpp
+++ Stack.cpp
@@ -122,11 +122,17 @@ int main()
             }
             break;
 
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+
+        default:
+            cout << "invalid choice. Try again." << endl;
+            break;
         }
 
         cout << endl;
     }
 
     return 0;
-
 };
