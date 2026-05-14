b2d7fa932e968d05f01036a88453587cb2676da0
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-05-14 11:16:47 +0700

    menambahkan case push

--- Stack.cpp
+++ Stack.cpp
@@ -94,7 +94,13 @@ int main()
 
         switch(choice)
         {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
 
+        
         }
 
         cout << endl;
